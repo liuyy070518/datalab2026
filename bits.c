34827aac9b00647b6e9d39999a1c17f178f0bd95
liuyy070518 <liuyy070518@gmail.com>
2026-09-23 02:17:17 +0000

    修改

--- bits.c
+++ bits.c
@@ -104,7 +104,7 @@ int byteSwap(int x, int n, int m) {
     int mb = (x >> ma) & 0xFF;
     int c = nb ^ mb;
 
-    return x ^ ((diff << na) | (diff << ma));
+    return x ^ ((x << na) | (x<< ma));
     
 }
 
